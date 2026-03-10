7f32166824c2fb40539026e4b0b5b51688c36796
Anderson Buitron <andersonb@hotmail.es>
2026-03-10 14:03:15 -0500

    Update summary in __manifest__.py

--- account_tax_certificate/__manifest__.py
+++ account_tax_certificate/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "13.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
-    "summary": "Genera certificados de retención/impuesto por tercero, impuesto y rango de fechas. 1234567",
+    "summary": "Genera certificados de retención/impuesto por tercero, impuesto y rango de fechas. 12345678",
     "author": "Ingenioso SAS",
     "website": "https://github.com/OCA/account-financial-tools",
     "depends": ["account"],
